620e40412d6a1a335049ca9851cc07349c47b5ad
Aidan Labourdette <layendan.h@gmail.com>
2022-02-15 10:47:42 -0700

    Changed episodes

--- svelte/src/lib/player/Episode.svelte
+++ svelte/src/lib/player/Episode.svelte
@@ -2,9 +2,43 @@
 	export let episode: any;
 </script>
 
+<hr class="solid" />
 <a href={episode.url}>
-	<img src={episode.thumbnail} alt={episode.title} />
-	<p>
-		{episode.title} on <i>{episode.site}</i>
-	</p>
+	<span>
+		<img src={episode.thumbnail} alt={episode.title} />
+		<p>
+			{episode.title} on <i>{episode.site}</i>
+		</p>
+	</span>
 </a>
+
+<style>
+	hr.solid {
+		margin-top: 20px;
+		/* border-top: 1px solid rgba(66, 66, 66, 0.5); */
+		border-color: #555;
+		/* box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5); */
+	}
+
+	span {
+		display: flex;
+		flex-direction: row;
+		margin-top: 1em;
+		margin-left: 2em;
+		margin-right: 2em;
+	}
+
+	img {
+		width: 100px;
+		height: auto;
+		aspect-ratio: 16/9;
+		margin-right: 2em;
+		border-radius: 5px;
+		box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+	}
+
+	a {
+		text-decoration: none;
+		color: #895ef4;
+	}
+</style>
--- svelte/src/lib/player/EpisodeHolder.svelte
+++ svelte/src/lib/player/EpisodeHolder.svelte
@@ -13,9 +13,14 @@
 			</li>
 		{/each}
 	</ul>
+	<hr class="solid" />
 </main>
 
 <style>
+	main {
+		margin: 0;
+		padding: 0;
+	}
 	.episode-list {
 		display: flex;
 		flex-direction: column;
@@ -23,5 +28,12 @@
 		list-style: none;
 		margin: 0;
 		padding: 0;
+	}
+
+	hr.solid {
+		margin-top: 20px;
+		/* border-top: 1px solid rgba(66, 66, 66, 0.5); */
+		border-color: #555;
+		/* box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5); */
 	}
 </style>

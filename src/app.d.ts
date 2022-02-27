344c94755d55a2e5b2b256c4a7edd58f49ba70c9
Aidan Labourdette <layendan.h@gmail.com>
2022-02-26 20:42:06 -0700

    I only need 1 file for it

--- src/app.d.ts
+++ src/app.d.ts
@@ -11,3 +11,11 @@
 
 // 	interface Stuff {}
 // }
+
+// Needed as error of injecting __TAURI__ into ts
+interface Window {
+  __TAURI__: {
+    // Could be changed to import in each file instead of doing it in whole project
+    [key: string]: any;
+  };
+}
--- src/shims.d.ts
+++
@@ -1,7 +0,0 @@
-// Needed as error of injecting __TAURI__ into ts
-interface Window {
-  __TAURI__: {
-    // Could be changed to import in each file instead of doing it in whole project
-    [key: string]: any;
-  };
-}

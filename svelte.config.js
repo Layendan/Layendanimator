eb274bea083bafd02945f95d764bb82a9bb9e0f1
Aidan Labourdette <layendan.h@gmail.com>
2022-04-07 02:19:39 -0700

    Updated and formatted files

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			// Tauri expects index.html, not app.html
-			fallback: 'index.html',
-		})
-	}
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      // Tauri expects index.html, not app.html
+      fallback: "index.html",
+    }),
+  },
 };
 
 export default config;
